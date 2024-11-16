0d394c19f1349ea64936c5c1511dd94410dd2f01
JFKongphop <kongphopleo@gmail.com>
2024-11-16 07:50:59 +0700

    connect web3auth

---
+++ web3-auth/src/abi.ts
@@ -0,0 +1,41 @@
+export const abi = [
+  {
+    "inputs": [],
+    "name": "getWord",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "newWord",
+        "type": "string"
+      }
+    ],
+    "name": "setWord",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "word",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+]
