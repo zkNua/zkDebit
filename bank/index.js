da525ff98f196953c9e8d82b9d0e3ce6ab4ba635
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2024-11-16 08:40:10 +0700

    adding api card setup

--- bank/index.js
+++ bank/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const snarkjs  = require('snarkjs') ; 
 
 const fs = require('fs');
 const cors = require('cors');
@@ -25,16 +26,40 @@ let cardsDataBase = [
 
 let cardAndProof = [];
 
-
 // 1. endpoint to send cardSetup.wasm and cardSetup_0000.zkey to the user
 app.get('/user/request/card-setup', (req, res) => {
-
+    res.zip([
+        { path: './cardSetup_0000.zkey', name: 'cardSetup_0000.zkey' },
+        { path: './cardSetup_js/cardSetup.wasm', name: 'cardSetup.wasm' }
+    ]);
 });
 
 // 2. Endpoint to user send proof and verify the setup proof from PrivacyVisa and keep in bank backend 
 app.post('/store-setup', async (req, res) => {
-    const { proof, public_output, _card_number } = req.body;
-    
+    const { proof, publicSignals, card_number } = req.body;
+    // check that the card number is not already stored
+    if (cardAndProof.find(card => card.card_number === card_number)) {
+        console.log("Card already stored");
+        res.status(400).json({ message: 'Card already stored', payload : "" });
+        return;
+    }
+    try {
+        const verificationKey = JSON.parse(fs.readFileSync("cardSetup_verification_key.json"));
+        // Verify proof 
+        const isValid = await snarkjs.groth16.verify(verificationKey, publicSignals, proof);
+        if (isValid) {
+            console.log("Setup proof verification successful");
+            console.log("public ", publicSignals[1]); 
+            cardAndProof.push({ card_number: card_number, pi3: publicSignals[1] });
+            res.status(200).json({ message: 'Proof verified and setup data stored' , payload : "" });
+        } else {
+            console.log("Setup proof verification failed");
+            res.status(400).json({ message: 'Invalid proof', payload : "" });
+        }
+    } catch (error) {
+        console.error("Error during setup verification:", error);
+        res.status(500).json({ message: 'Error during setup verification', payload : error.toString() });
+    }
 });
 
 // 3. Endpoint to create a transaction order from shop initialize onchain
