809d35aa3e14e22a71858e2bc5c6978a1d093e2c
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2024-12-11 16:35:59 +0700

    adding api creating transaction

---
+++ shop/src/app/api/transaction/create/route.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import CreatingTransaction from "../../../../services/creatingTransaction";
+
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+    const { pi3, goods } = body;
+    console.log("Received data:", pi3, goods);
+    CreatingTransaction(pi3, goods);
+
+    return NextResponse.json(
+        {
+            success: true,
+            message: "Data received successfully!" 
+        },
+        {
+            status: 200
+        }
+    )
+  } catch (error: any) {
+    // Handle errors gracefully
+    console.error("Error in POST handler:", error);
+    return NextResponse.json(
+      { success: false, error: error.message },
+      { status: 500 }
+    )
+  }
+}
