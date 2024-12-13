0eb7ae5df9f84c0d47f3b6057ef8663c5f5741f1
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2024-12-13 10:08:24 +0700

    refactor creatint tx services

--- shop/src/app/api/transaction/create/route.ts
+++ shop/src/app/api/transaction/create/route.ts
@@ -1,13 +1,36 @@
+
 import { NextRequest, NextResponse } from "next/server";
 
-import CreatingTransaction from "../../../../services/creatingTransaction";
-
-export async function POST(req: NextRequest) {
+export async function POST(req: NextRequest, res : NextResponse) {
   try {
     const body = await req.json();
-    const { pi3, goods } = body;
+    const { pi3, _amount , transaction_hashed } = body;
+    console.log(pi3, _amount , transaction_hashed)
+    // Host send to bank 
+    await fetch("http://localhost:4000/create-transaction", {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        pi3: pi3,
+        amount: _amount,
+        transaction_hashed: transaction_hashed
+      })
+  })
 
-    CreatingTransaction(pi3, goods);
+  // await fetch("https://zkdebit-host/create-transaction", {
+  //     method: "POST",
+  //     headers: {
+  //         "Content-Type": "application/json"
+  //     },
+  //     body: JSON.stringify({
+  //         transaction_hashed: transactionHashed,
+  //         amount: goods.amount,
+  //         check_pi3: goods.ppp
+  //     })
+  // })
+
     return NextResponse.json(
         {
             success: true,
--- shop/src/services/creatingTransaction.ts
+++ shop/src/services/creatingTransaction.ts
@@ -1,42 +1,42 @@
 'use server'
+import { error } from "node:console";
 import { iGoodPayload } from "../interface/transaction";
 import crypto from 'crypto';
 
 // Creating transaction hashed include with Pi3 & amount to both bank host & zkdebit host
 export default async function CreatingTransaction(
     pi3: string,
-    goods: iGoodPayload
+    goods: iGoodPayload[]
 ){
+    try{
+        if (!pi3 || typeof pi3 !== "string" ){
+            throw new Error ("Invalid proof of your card setup phase");
+        }
+        if (goods.length === 0 ) { 
+            throw new Error ("Goods is missing ")
+        }
+        const totalPrice = goods.reduce((sum, item) => sum + item.ppp * item.amount, 0);         
+        const stringGoods = JSON.stringify(goods);
+        const transactionHashed = crypto
+        .createHash("sha256")
+        .update(`${stringGoods} ${pi3}`)
+        .digest("hex")
 
-    const stringGoods = JSON.stringify(goods);
-    const transactionHashed = crypto
-    .createHash("sha256")
-    .update(`${stringGoods} ${pi3}`)
-    .digest("hex")
-
-    // Host send to bank 
-    await fetch("http://localhost:4000/create-transaction", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            transaction_hashed: transactionHashed ,
-            amount: goods.amount,
-            check_pi3: pi3
+        const response = await fetch(`https://localhost:3000/api/transaction/create`,{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                transaction_hashed: transactionHashed ,
+                amount: totalPrice,
+                pi3: pi3
+            })
         })
-    })
+        return response
 
-    // await fetch("https://zkdebit-host/create-transaction", {
-    //     method: "POST",
-    //     headers: {
-    //         "Content-Type": "application/json"
-    //     },
-    //     body: JSON.stringify({
-    //         transaction_hashed: transactionHashed,
-    //         amount: goods.amount,
-    //         check_pi3: goods.ppp
-    //     })
-    // })
+    }catch (err){
+        console.log(err)
+    }
     console.log("Transaction created successfully!")
 }
