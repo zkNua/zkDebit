87b78c1bc3007a48d1d7e90259c3590dfb2dc998
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2025-01-28 01:42:55 +0700

    remove un-used files

--- zkdebit/src/utils/bitkubchain-sdk.ts
+++
@@ -1,7 +0,0 @@
-import { Network, initializeSDK } from "@bitkub-chain/sdk.js";
-
-export const sdk = initializeSDK(
-  "6737d7cb789b97001c640d8c", // Client ID
-  "sdk-e7750bb3-424a-44d0-8666-7216982ca107", // Project ID
-  Network.BKC_TESTNET
-);
